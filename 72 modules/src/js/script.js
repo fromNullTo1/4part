8f0fbbbed6b16a3d77e28b21a7a1e6eebefaf507
Wadzim <wadzim.dev@gmail.com>
2022-09-15 20:47:28 +0300

    feat: modules

---
+++ 72 modules/src/js/script.js
@@ -0,0 +1,13 @@
+// import {one, two} from './main';
+// import {one as first} from './main';
+import sayHi from './main';
+
+// import * as data from './main';
+
+// console.log(`${one} and ${two}`);
+// console.log(first);
+
+
+// console.log(`${data.one} and ${data.two}`);
+// data.sayHi();
+sayHi();
